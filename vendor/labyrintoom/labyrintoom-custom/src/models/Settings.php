84cf6990870a7a9354ca6ecedc777c30d519ef50
SamiullahMahjoor <mahjoor_samiullah@hotmail.com>
2021-07-03 18:02:10 +0500

    Setup completed
    Done.

--- vendor/labyrintoom/labyrintoom-custom/src/models/Settings.php
+++ vendor/labyrintoom/labyrintoom-custom/src/models/Settings.php
@@ -28,7 +28,7 @@ class Settings extends Model
     /**
      * @var string
      */
-    public $someAttribute = 'Some Default';
+    public $site_title = 'Some Default';
 
     // Public Methods
     // =========================================================================
@@ -39,8 +39,8 @@ class Settings extends Model
     public function rules()
     {
         return [
-            ['someAttribute', 'string'],
-            ['someAttribute', 'default', 'value' => 'Some Default'],
+            ['site_title', 'string'],
+            ['site_title', 'default', 'value' => 'Some Default'],
         ];
     }
 }
